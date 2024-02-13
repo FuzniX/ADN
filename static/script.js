10257ded7caaa060403d155d421250b8740462ee
FuzniX <68467915+FuzniX@users.noreply.github.com>
2024-02-13 08:22:51 +0000

    a

--- static/script.js
+++ static/script.js
@@ -20,3 +20,49 @@ function conseils() {
     spanConseils.textContent = conseils[indiceAleatoire];
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    var backButton = document.getElementById("back-to-top");
+
+    window.addEventListener("scroll", function () {
+        // Afficher ou masquer le bouton en fonction du défilement
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backButton.style.display = "block";
+        } else {
+            backButton.style.display = "none";
+        }
+    });
+
+    backButton.addEventListener("click", function () {
+        // Appel de la fonction pour faire défiler la page avec une animation
+        scrollToTop(1000); // 1000 milliseconds (1 seconde) pour l'animation
+    });
+
+    // Fonction pour faire défiler la page vers le haut avec une animation
+    function scrollToTop(duration) {
+        var start = document.body.scrollTop || document.documentElement.scrollTop;
+        var startTime = new Date().getTime();
+
+        var animateScroll = function () {
+            var now = new Date().getTime();
+            var timeElapsed = now - startTime;
+
+            document.body.scrollTop = document.documentElement.scrollTop = easeInOut(timeElapsed, start, -start, duration);
+
+            if (timeElapsed < duration) {
+                requestAnimationFrame(animateScroll);
+            } else {
+                document.body.scrollTop = document.documentElement.scrollTop = 0;
+            }
+        };
+
+        requestAnimationFrame(animateScroll);
+    }
+
+    // Fonction d'accélération/décélération (EaseInOut)
+    function easeInOut(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+});
