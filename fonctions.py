899a895bf2d96b29aa402ed5398de9b1a13fbf76
FuzniX <dupontnicolas703@gmail.com>
2024-02-13 22:31:38 +0100

    Finalizing everything + added * Search

--- fonctions.py
+++ fonctions.py
@@ -2,27 +2,22 @@ from flask.sansio.scaffold import T_route
 from assets import Assets as a
 
 
-def get_lettre(triplet: str) -> str:
-    """
-    Renvoie la lettre correspondant au triplet d'un code génétique donné
-    """
-    return a.DICO2.get(triplet, "Z")
-
-
 def in_string(mot: str, chaine: str) -> str:
     """
     Retourne une chaine de caractères entourant le mot donné dans le génome donné.
     ...404 - NOT FOUND... si non trouvée.
 
     """
-    index = chaine.find(mot)
+    index = recherche_mot(chaine, mot)
+
+    mot_dans_sequence = chaine[index: index + len(mot)]
     
     if 0 <= index < 16:
-        return (chaine[:index + 18] + "...").replace(mot, f" - {mot} - ")
+        return (chaine[:index + 18] + "...").replace(mot_dans_sequence, f" - {mot_dans_sequence} - ", 1)
     elif len(chaine) - 19 < index:
-        return ("..." + chaine[index - 15:]).replace(mot, f" - {mot} - ")
+        return ("..." + chaine[index - 15:]).replace(mot_dans_sequence, f" - {mot_dans_sequence} - ", 1)
     elif index >= 16:
-        return ("..." + chaine[index - 15:index + 18] + "...").replace(mot, f" - {mot} - ")
+        return ("..." + chaine[index - 15:index + 18] + "...").replace(mot_dans_sequence, f" - {mot_dans_sequence} - ", 1)
     else:
         print(index)
         return "...404 - NOT FOUND..."
@@ -34,9 +29,20 @@ def transposition(texte: str) -> str:
     """
     texte_transpose = ""
     for i in range(0, len(texte) // 3 * 3, 3):
-        lettre_tempo = texte[i:i + 3]
-        texte_transpose += get_lettre(lettre_tempo)
+        triplet = texte[i:i + 3]
+        texte_transpose += a.DICO2.get(triplet, "Z")
     return texte_transpose
+
+def transposition_etoile(texte: str) -> str:
+    """
+    Reçoit un code génétique en str et renvoie le code transposé par le dico
+    """
+    texte_transpose = ""
+    for i in range(0, len(texte) // 3 * 3, 3):
+        triplet = texte[i:i + 3]
+        texte_transpose += a.DICO.get(triplet, "Z")
+    return texte_transpose
+
 
 
 def fic_to_txt(file_name: str) -> str:
@@ -48,6 +54,9 @@ def fic_to_txt(file_name: str) -> str:
 
 
 def get_set():
+    """
+    Retourne un set de tous les mots du fichier MOTS.txt
+    """
     with open("static/MOTS.txt", "r") as f:
         return {mot.strip() for mot in f.readlines()}
 
@@ -70,9 +79,30 @@ def is_valid(file_name: str) -> bool:
 
 
 def fna_to_txt(file_name: str) -> None:
+    """
+    Convertit un fichier FNA (format de base pour NBCI) en fichier TXT.
+    Les lignes de description du génome sont retirées.
+    """
     with open(file_name, "r") as fic:
         lignes = [ligne for ligne in fic.readlines() if all(char in ["A", "T", "C", "G", "N", "\n"] for char in ligne)]
     file_name = f"{file_name[:-3]}txt"
 
     with open(file_name, "w") as f:
         f.write("".join(lignes))
+
+def recherche_mot(sequence: str, mot: str) -> int:
+    """
+    Recherche un mot dans une séquence transposée du génome en prenant en compte l'étoile comme joker.
+    Retourne l'indice de la première occurence du mot s'il est trouvé, sinon -1.
+    """
+    longueur_mot = len(mot)
+    for i in range(len(sequence) - longueur_mot + 1):
+        sous_sequence = sequence[i:i + longueur_mot]
+        match = True
+        for j in range(longueur_mot):
+            if mot[j] != "*" and sous_sequence[j] != mot[j] and sequence[i+j] != "*":
+                match = False
+                break
+        if match:
+            return i
+    return -1
--- main.py
+++ main.py
@@ -7,22 +7,15 @@ from unidecode import unidecode
 
 app = Flask(__name__)
 
-
+# Accueil
 @app.route('/')
 def index():
     return render_template("index.html", disp="none")
 
-
-# Accueil du site
-@app.route('/index.html')
-def affiche_index():
-    return render_template("index.html", disp="none")
-
-
 # Recevoir un fichier et retourner la liste des mots présents dans le génome donné
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    global gen
+    global gen, gen_etoile  # Variables globales des gènes à traiter
     global mots  # Variable globale de la liste des mots dans le génome triée dans l'ordre alphabétique
 
     # Récupérer le fichier
@@ -30,17 +23,25 @@ def upload():
 
     if not file:
         return render_template("index.html", disp=True)
-
     nom_fichier = 'static/' + secure_filename(file.filename)
     file.save(nom_fichier)
 
+    # Vérifier que le fichier est valide
     if not f.is_valid(nom_fichier):
         return render_template("index.html", disp=True)
 
-    gen = f.transposition(f.fic_to_txt(nom_fichier))
-    t1 = time.time()
-    mots = sorted({(mot, gen.find(mot)) for mot in f.get_set() if mot in gen}, key=lambda x: x[1])  # POSE PROBLEME
-    print(time.time()-t1)
+    gen = f.transposition(f.fic_to_txt(nom_fichier)) # Le gène sous forme transformée avec des lettres prédifinies pour récupérer une liste de mots
+    gen_etoile = f.transposition_etoile(f.fic_to_txt(nom_fichier)) # Gène brut où les * seront traités comme n'importe quel caractère
+
+    # t1 = time.time() # COMPLEXITE DE L'AFFICHAGE DE TOUS LES MOTS PRESENTS DANS LE GENOME
+    if request.form.get('afficherMots') == "True":
+        # Si l'utilisateur décide d'afficher les mots
+        mots = sorted({(mot, gen.find(mot)) for mot in f.get_set() if mot in gen}, key=lambda x: x[1])  # POSE PROBLEME
+    else:
+        # Si l'utilisateur décide de ne pas afficher les mots, gagne un temps proportionnel à la longueur du génome
+        mots = set()
+
+    # print(time.time()-t1) # AFFICHAGE DE LA COMPLEXITE
 
     return render_template("chercheur.html", les_mots=mots, status="", phrase="-1")
 
@@ -53,70 +54,57 @@ def rechercher():
     if phrase:
         phrase = unidecode(phrase.upper())
     else:
+        # Cas où aucune phrase n'est donnée.
         return render_template("chercheur.html",
                                les_mots=mots,
                                status="Aucune phrase n'a été donnée.",
                                phrase="-1",)
 
-    # try:
-    if True:
-
+    try:
+        # Initialisation de variables utilisées dans la boucle for suivante
         mots_sans_indice = (mot[0] for mot in mots)
         liste_phrase = phrase.split(" ")
-        gen_tempo = gen
+        gen_tempo = gen_etoile
         chaine = []
-        
 
         for i, el in enumerate(liste_phrase):
-            if el in gen_tempo:
+            if f.recherche_mot(gen_tempo, el) != -1:
+                # Formatage de l'affichage de la séquence de caractères
                 string = f.in_string(el, gen_tempo)
 
+                # Cas où il n'y a pas 19 caractères avant le premier tiret et où il y a répétition
                 if string.find("-") != 19 and i != 0:
-                    repet = string.split()[0]
-                    apres_la_derniere = chaine[-1].split()[-1]
+                    chaine[-1] = chaine[-1][:-21] + "- " + string
 
-                    if (indice := apres_la_derniere.find(repet)) != -1:
-                        apres_la_derniere = apres_la_derniere[:indice + len(repet)]
-                        chaine[-1] = chaine[-1].split()[-1][:-1] + apres_la_derniere[:-3] + string.split()[2:]
-                        print(chaine[-1].split()[-1][:-1])
-                        print(apres_la_derniere[:-3])
-                        print(string.split()[2:])
-                    else:
-                        print(chaine[-1][:-3])
-                        print(string)
-                        chaine[-1] = chaine[-1][:-21] + string
-
-                    
-                        
-                    # if chaine[-1][19] == "-":
-                    #     pre = chaine[-1][-15:]
+                # Cas où il n'y a pas de répétition
                 else:
                     chaine.append(string)
                 
                 gen_tempo = gen_tempo[gen_tempo.find(el) + len(el):]
                               
             elif el in mots_sans_indice:
+                # Les mots qui constituent la phrase ne sont pas dans l'ordre dans le génome.
                 return render_template("chercheur.html",
                                        les_mots=mots,
                                        status=f"Les mots qui constituent la phrase ne sont pas dans l'ordre dans le génome.",
                                        phrase="-1",)
             else:
+                # Un élément n'est pas dans le génome
                 return render_template("chercheur.html",
                                        les_mots=mots,
                                        status=f"{el} n'est pas dans le génome.",
                                        phrase="-1")
 
-        
+        # La phrase est dans le génome
         return render_template("chercheur.html",
                                les_mots=mots,
                                status=f"La phrase '{phrase.capitalize()}' est dans le génome.",
                                phrase=liste_phrase,
                                chaine=chaine)
 
-    # Dans le cas où il y a une exception, l'afficher à l'utilisateur
-    # except Exception as e:
-    #     print(e)
-    #     return index()
+    # Dans le cas où il y a une exception, retourner à l'accueil
+    except: return index()
 
 
+# Démarrer le serveur Web
 app.run(host="0.0.0.0", port=26008)
