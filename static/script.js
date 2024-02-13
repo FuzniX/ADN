d25fafff5e7aa8d9e3cf58c341100c33dc8896c3
FuzniX <68467915+FuzniX@users.noreply.github.com>
2024-02-13 07:29:34 +0000

    a

--- static/script.js
+++ static/script.js
@@ -1,4 +1,22 @@
 function demarrerTraitement() {
     document.getElementById("chargement").style.display = "block";
     document.getElementById("background").style.display = "none";
+    setInterval(conseils, 2500)
 }
+
+
+
+function conseils() {
+    const conseils = [
+        "VERIFICATION ANTIGOUVERNEMENTALE...",
+        "LA TERRE EST PLATE...",
+        "ANALYSE G8N0ME...",
+        "MAIS OU SONT LES REPTILIENS...",
+        "ARRÊTEZ..."
+    ]
+    
+     spanConseils = document.getElementById("conseils");
+    const indiceAleatoire = Math.floor(Math.random() * conseils.length);
+    spanConseils.textContent = conseils[indiceAleatoire];
+}
+
